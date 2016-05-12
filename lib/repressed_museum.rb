6b72ddafb5f1660ef178cd4c6130ad4a2b6c545c
Vasilis Spilka <vasspilka@gmail.com>
2016-05-12 14:09:55 +0300

    hanami new

---
+++ lib/repressed_museum.rb
@@ -0,0 +1,58 @@
+require 'hanami/model'
+require 'hanami/mailer'
+Dir["#{ __dir__ }/repressed_museum/**/*.rb"].each { |file| require_relative file }
+
+Hanami::Model.configure do
+  ##
+  # Database adapter
+  #
+  # Available options:
+  #
+  #  * Memory adapter
+  #    adapter type: :memory, uri: 'memory://localhost/repressed_museum_development'
+  #
+  #  * SQL adapter
+  #    adapter type: :sql, uri: 'sqlite://db/repressed_museum_development.sqlite3'
+  #    adapter type: :sql, uri: 'postgres://localhost/repressed_museum_development'
+  #    adapter type: :sql, uri: 'mysql://localhost/repressed_museum_development'
+  #
+  adapter type: :sql, uri: ENV['REPRESSED_MUSEUM_DATABASE_URL']
+
+  ##
+  # Migrations
+  #
+  migrations 'db/migrations'
+  schema     'db/schema.sql'
+
+  ##
+  # Database mapping
+  #
+  # Intended for specifying application wide mappings.
+  #
+  # You can specify mapping file to load with:
+  #
+  # mapping "#{__dir__}/config/mapping"
+  #
+  # Alternatively, you can use a block syntax like the following:
+  #
+  mapping do
+    # collection :users do
+    #   entity     User
+    #   repository UserRepository
+    #
+    #   attribute :id,   Integer
+    #   attribute :name, String
+    # end
+  end
+end.load!
+
+Hanami::Mailer.configure do
+  root "#{ __dir__ }/repressed_museum/mailers"
+
+  # See http://hanamirb.org/guides/mailers/delivery
+  delivery do
+    development :test
+    test        :test
+    # production :stmp, address: ENV['SMTP_PORT'], port: 1025
+  end
+end.load!
