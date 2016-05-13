c9fa141e8c24a2499cfd5eb2e7bccaef5960a807
Vasilis Spilka <vasspilka@gmail.com>
2016-05-13 20:02:33 +0300

    create messages

--- apps/web/controllers/messages/create.rb
+++ apps/web/controllers/messages/create.rb
@@ -2,7 +2,25 @@ module Web::Controllers::Messages
   class Create
     include Web::Action
 
+    params do
+      param :message do
+        param :title
+        param :text
+      end
+    end
+
     def call(params)
+      MessageRepository.create Message.new(message_params)
+      redirect_to '/'
+    end
+
+    private
+
+    def message_params
+      {
+        text:  params[:message][:text],
+        title: params[:message][:title]
+      }
     end
   end
 end
