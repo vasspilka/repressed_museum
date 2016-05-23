7b11c062c601e3a895611b38f5b7a6e8924868d6
Vasilis Spilka <vasspilka@gmail.com>
2016-05-23 15:40:34 +0300

    i18n for form

--- apps/web/views/messages/repress.rb
+++ apps/web/views/messages/repress.rb
@@ -1,5 +1,27 @@
 module Web::Views::Messages
   class Repress
     include Web::View
+
+    def form
+      form_for :message, '/messages' do
+
+        div class: 'controls' do
+          label      locale["form"]["title"], class: 'label'
+          text_field :title, class: 'input'
+        end
+
+        div class: 'controls' do
+          label      locale["form"]["text"], class: 'label'
+          text_area  :text, class: 'input'
+        end
+
+        div class: 'controls' do
+          submit     locale["form"]["submit"], class: 'button'
+        end
+
+      end
+    end
+
+
   end
 end
